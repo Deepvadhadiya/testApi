77298885054644acf6ee5e4b323ef4e9b8757aa3
Deepvadhadiya <deepvadhadiya@gmail.com>
2023-03-07 12:43:28 +0530

    testApi

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+// const cros = require("cros");
+const app = express();
+
+// app.use(cros());
+
+const apiData = require("./data.json");
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello i am live");
+})
+
+app.get("/service", (req, res) => {
+    res.send(apiData);
+})
+
+app.listen(port , () => {
+    console.log("I am live again");
+})
